b04763e328c799afa7635ed6c7b3fd28e9f1d468
Eric Ekström <ericekstrm@gmail.com>
2023-12-13 20:19:49 +0100

    dag 13 2023.

--- 2023/13.py
+++ 2023/13.py
@@ -1,28 +1,28 @@
-
-
-
-
 def flip(l):
-    return list(zip(*l))
-    
+    return ["".join(x) for x in list(zip(*l))]
 
+def find_symmetry(l):
+    for i in range(1, len(l)):
+        dist = min(i, len(l) - i)
+        left = l[i-dist:i]
+        right = l[i:i+dist]
 
-def convert_to_ints(pattern):
-    binarys = []
-    for line in pattern:
-        binarys.append(int("".join(["1" if c == "#" else "0" for c in line]), 2))
-    return binarys
-
-
+        smudges = 0
+        for x, y in zip("".join(left), "".join(right[::-1])):
+            if x != y:
+                smudges += 1
+        if smudges == 1:
+            return i
+    return 0
 
 with open("13.in") as input_file:
     contents = input_file.read()
     contents = contents.split("\n\n")
-
     patterns = [s.strip().split("\n") for s in contents]
-    pattern = patterns[0]
-    print(pattern)
-    print(convert_to_ints(pattern))
-    pattern = flip(pattern)
-    print(convert_to_ints(pattern))
-    
+
+total = 0
+for pattern in patterns:
+    total += 100 * find_symmetry(pattern)
+    total += find_symmetry(flip(pattern))
+print(total)
+
